63e5ca88679ee1a1d364d877c35c8be3d11a4a04
hiepnguyenhuy <ks.huyhiep@gmail.com>
2022-10-06 11:38:14 +0700

    nop bai thuc hanh 7

---
+++ bai7_thuchanh_khaibaovasudungbien.js
@@ -0,0 +1,26 @@
+let i=10
+let f=20.5
+let b=true
+let s='Hà Nội'
+
+document.write('i =' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+document.write('<br/>')
+
+let width = 20
+let height = 10
+let area = width*height
+document.write('Area = ' + area)
+document.write('<br/>')
+c = prompt("Nhập biến c: ");
+d = prompt("Nhập biến d: ");
+if (c%d ==0) {
+    alert("c chia hết cho d");
+} else {
+    alert("c không chia hết cho d");
+}
